3c0bd6acd3de842dd1be415566b81c854c790d5c
Jokūbas Rybelis <frizjr@protonmail.com>
2023-02-23 15:02:47 +0200

    Seed default country and hotel data. Update database.

--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -11,4 +11,82 @@ public class HotelListingDbContext : DbContext
 
     public  DbSet<Hotel> Hotels { get; set; }
     public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Country>().HasData(
+            new Country
+            {
+                Id = 1,
+                Name = "New Zealand",
+                ShortName = "NZ"
+            },
+            new Country
+            {
+                Id = 2,
+                Name = "Colombia",
+                ShortName = "CO"
+            },
+            new Country
+            {
+                Id = 3,
+                Name = "Jamaica",
+                ShortName = "JM"
+            },
+            new Country
+            {
+                Id = 4,
+                Name = "Italy",
+                ShortName = "IT"
+            },
+            new Country
+            {
+                Id = 5,
+                Name = "India",
+                ShortName = "ID"
+            });
+
+        modelBuilder.Entity<Hotel>().HasData(
+            new Hotel
+            {
+                Id = 1,
+                Name = "Comfort Suites",
+                Address = "Bogota DC",
+                CountryId = 2,
+                Rating = 3.9
+            },
+            new Hotel
+            {
+                Id = 2,
+                Name = "Mercure",
+                Address = "Wellington",
+                CountryId = 1,
+                Rating = 4.3
+            },
+            new Hotel
+            {
+                Id = 3,
+                Name = "Anantara Rasananda",
+                Address = "Mumbai",
+                CountryId = 5,
+                Rating = 4.9
+            },
+            new Hotel
+            {
+                Id = 4,
+                Name = "Sandals Resort and Spa",
+                Address = "Negril",
+                CountryId = 3,
+                Rating = 4.5
+            },
+            new Hotel
+            {
+                Id = 5,
+                Name = "NH",
+                Address = "Rome",
+                CountryId = 4,
+                Rating = 4.0
+            });
+    }
 }
