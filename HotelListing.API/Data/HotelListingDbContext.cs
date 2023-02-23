f8e650643135ab2b64b75ff5a31114e375d6bbfd
Jokūbas Rybelis <frizjr@protonmail.com>
2023-02-23 10:36:58 +0200

    Add entity framework, setup db connection, add db context class.

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data;
+
+public class HotelListingDbContext : DbContext
+{
+    public HotelListingDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
