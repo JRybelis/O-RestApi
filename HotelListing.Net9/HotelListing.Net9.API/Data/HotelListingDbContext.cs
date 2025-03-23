36e062901793f8dfc5431840a308377a088e30f6
JokūbasR <frizjr@protonmail.com>
2025-03-23 19:23:02 +0200

    Seed default country and hotel data. Set migrations up and update the DB

--- HotelListing.Net9/HotelListing.Net9.API/Data/HotelListingDbContext.cs
+++ HotelListing.Net9/HotelListing.Net9.API/Data/HotelListingDbContext.cs
@@ -6,5 +6,41 @@ public class HotelListingDbContext(DbContextOptions options) : DbContext(options
 {
     public DbSet<Hotel> Hotels { get; set; }
     public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Country>().HasData(
+            new Country
+            {
+                Id = 1,
+                Name = "Japan",
+                ISOCode = "JPN"
+            },
+            new Country
+            {
+                Id = 2,
+                Name = "Slovakia",
+                ISOCode = "SVK"
+            });
+
+        modelBuilder.Entity<Hotel>().HasData(
+            new Hotel
+            {
+                Id = 1,
+                Name = "Mariott",
+                Address = "96 Shinto street, Osaka-ku",
+                Rating = 4.5,
+                CountryId = 1,
+            },
+            new Hotel
+            {
+                Id = 2,
+                Name = "NH",
+                Address = "97 Slava ul., Bratislava",
+                Rating = 3.5,
+                CountryId = 2
+            });
+    }
 }
 
---
+++ HotelListing.Net9/HotelListing.Net9.API/Migrations/20250323171755_SeededCountriesAndHotels.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.Net9.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededCountriesAndHotels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "ISOCode", "Name" },
+                values: new object[,]
+                {
+                    { 1, "JPN", "Japan" },
+                    { 2, "SVK", "Slovakia" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "96 Shinto street, Osaka-ku", 1, "Mariott", 4.5 },
+                    { 2, "97 Slava ul., Bratislava", 2, "NH", 3.5 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
