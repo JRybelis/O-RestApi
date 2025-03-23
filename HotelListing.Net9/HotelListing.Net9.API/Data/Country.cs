cd8dddb740f8262ce844c3d1f14014ae6047258a
JokūbasR <frizjr@protonmail.com>
2025-03-23 19:05:41 +0200

    Add country and hotel entities, setup relationships and run initial migration

---
+++ HotelListing.Net9/HotelListing.Net9.API/Data/Country.cs
@@ -0,0 +1,10 @@
+namespace HotelListing.Net9.Data;
+
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ISOCode { get; set; }
+
+    public virtual IList<Hotel> Hotels { get; set; }
+}
